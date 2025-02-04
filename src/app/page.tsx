feb7eef83c779b644899fc8d3168ee8b2f141d46
SavostinIllia <savostin.illia@gmail.com>
2025-02-04 21:35:20 +0100

    random characters fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import CharacterList from "@/components/CharacterList/CharacterList";
 import SkeletonList from "@/components/SkeletonLoader/SkeletonLoader";
 import { Suspense } from "react";
 
+export const dynamic = 'force-dynamic';
 
- function RickMortyHeroPage (){
+function RickMortyHeroPage (){
 
-  return (
+return (
     <div>
       <Suspense fallback={<SkeletonList items={6}/>}>
           <CharacterList randompage={true}/>
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -23,17 +23,17 @@ async function  Footer () {
 
   return (
     <footer className="my-4 text-center flex items-center justify-center gap-5 w-full absolute bottom-0 left-0">
-      <Link href={`${URL_PATHS.character}`}>
+      <Link href={`/${URL_PATHS.character}`}>
         <Typography tag="p" className="font-bold text-xl ricks-text flex">
         Characters: {characters?.info?.count ?? 'N/A'} {fetchError && <Image src={pickle} width={20} height={10} alt="pickle error" className='mr-1'/>}
         </Typography>
       </Link>
-      <Link href={`${URL_PATHS.episode}`}>
+      <Link href={`/${URL_PATHS.episode}`}>
         <Typography tag="p" className="font-bold text-xl ricks-text flex">
           Episodes: {episodes?.info.count ?? 'N/A'} {fetchError && <Image src={pickle} width={20} height={10} alt="pickle error" className='mr-1'/>}
         </Typography>
       </Link>
-      <Link href={`${URL_PATHS.location}`}>
+      <Link href={`/${URL_PATHS.location}`}>
         <Typography tag="p" className="font-bold text-xl ricks-text flex">
           Locations: {locations?.info?.count?? 'N/A'} {fetchError && <Image src={pickle} width={20} height={10} alt="pickle error" className='mr-1'/>}
         </Typography>
